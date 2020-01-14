325eaa386d04fcb86245bf6ee7c11f1eb4056c60
danwakeem <danwakeem@gmail.com>
2020-01-14 10:19:28 -0600

    Updated schema generator

--- generators/schema/index.js
+++ generators/schema/index.js
@@ -1,40 +1,50 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-const _ = require('lodash');
-const fs = require('fs');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const _ = require("lodash");
+const fs = require("fs");
 
 module.exports = class extends Generator {
   async prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(`Welcome to the transcendent ${chalk.red('generator-apollo-knex')} generator!`)
+      yosay(
+        `Welcome to the transcendent ${chalk.red(
+          "generator-apollo-knex"
+        )} generator!`
+      )
     );
 
     this.answers = await this.prompt([
       {
         type: "input",
         name: "name",
-        message:
-          "File name for your sample schema file",
+        message: "File name for your sample schema file",
         default: "sample"
-      },
+      }
+    ]);
+    const secondAnswers = await this.prompt([
       {
         type: "input",
         name: "apiName",
         message:
           "What would you like the name of the first sample schema to be? (You will probably replace this but just here to get you started)",
-        default: "sample"
+        default: this.answers.name
       },
       {
         type: "input",
         name: "tableName",
         message:
           "This is the table name that will map to your sample graphql schema.",
-        default: "sample"
-      },
+        default: this.answers.name
+      }
     ]);
+
+    this.answers = {
+      ...this.answers,
+      ...secondAnswers
+    };
 
     this.answers.apiNameCap = _.capitalize(this.answers.apiName);
   }
@@ -42,30 +52,34 @@ module.exports = class extends Generator {
   writing() {
     this.fs.copyTpl(
       this.templatePath("sample"),
-      this.destinationPath(`src/schemas/${this.answers.name}/${this.answers.name}.ts`), 
+      this.destinationPath(
+        `src/schemas/${this.answers.name}/${this.answers.name}.ts`
+      ),
       {
-        ...this.answers,
+        ...this.answers
       }
     );
 
     this.fs.copyTpl(
       this.templatePath("sample.spec"),
-      this.destinationPath(`src/schemas/${this.answers.name}/${this.answers.name}.spec.ts`),
+      this.destinationPath(
+        `src/schemas/${this.answers.name}/${this.answers.name}.spec.ts`
+      ),
       {
-        ...this.answers,
+        ...this.answers
       }
     );
 
     if (fs.existsSync("src/schemas/index.ts")) {
       const path = "src/schemas/index.ts";
       const content = this.fs.read(path);
-      const replace = `, ${this.answers.name} };`
+      const replace = `, ${this.answers.name} };`;
       const importReplace = `\nimport * as ${this.answers.name} from './${this.answers.name}/${this.answers.name}';\n\nexport`;
       const importContent = content.replace(/\s*export/gim, importReplace);
-      const newContent = importContent.replace(/(,\s*|\s*)};/gmi, replace);
+      const newContent = importContent.replace(/(,\s*|\s*)};/gim, replace);
       this.fs.write(path, newContent);
     } else {
-      const fileString = `import * as ${this.answers.name} from './${this.answers.name}/${this.answers.name}';\n\nexport { ${this.answers.name} };\n`
+      const fileString = `import * as ${this.answers.name} from './${this.answers.name}/${this.answers.name}';\n\nexport { ${this.answers.name} };\n`;
       this.fs.write("src/schemas/index.ts", fileString);
     }
   }
